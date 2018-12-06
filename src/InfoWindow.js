08d44b07f62ac28f22280ed7b86fb7e93a0bbeba
gb015j <gb015j@att.com>
2018-12-05 19:08:22 -0800

    Moving project files to github

---
+++ src/InfoWindow.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Draggable from 'react-draggable';
+
+class InfoWindow extends Component {
+  render() {
+    const { place } = this.props;
+
+    return (
+    <Draggable>
+      <article className='info-window' role='article' tabIndex='1'>
+        <h2 className='info-name'>{place.name}</h2>
+        <p
+          onClick={() => {this.props.hideInfoWindow()}}
+          className='close-window'>X</p>
+        <p className='info-category'>{place.categories[0].name}</p>
+        <p className='info-address'>{place.location.address}, {place.location.city}</p>
+        <p className='info-rating'>Rating: {place.rating} ({place.likes.summary})</p>
+        {place.bestPhoto && (
+          <img
+            arial-label={place.name}
+            alt={place.name}
+            src={`${place.bestPhoto.prefix}300x200${place.bestPhoto.suffix}`}
+            onDragStart={event => event.preventDefault()}></img>
+        )}
+      </article>
+    </Draggable>
+    )
+  }
+}
+
+export default InfoWindow;
---
+++ src/Map.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+
+/**
+ * Set value to true on map load callback
+ */
+function onMapLoaded() {
+  console.log('map callback');
+  window.isMapLoaded = true;
+}
+
+/**
+ * Map component itself
+ */
+const MapComponent = withScriptjs(withGoogleMap(props => {
+    return <GoogleMap
+      defaultZoom={12}
+      defaultCenter={props.places.length > 0 ? props.places[0] : {lat: 47.605594, lng: -122.330770}}
+      defaultOptions={{mapTypeControl: false}}
+      onClick={props.hideInfoWindow}
+      >
+      {props.isMarkerShown && (props.places.map((place, index) =>
+        <Marker
+          key={index}
+          position={place}
+          animation={place.clicked ?
+            window.google.maps.Animation.BOUNCE : 0}
+          onClick={() => {props.onMarkerClick(index)}} /> ))
+      }
+    </GoogleMap>
+  }
+))
+
+/**
+ * Map container with map component inside
+ */
+class Map extends Component {
+  componentDidMount() {
+    // Set initial value
+    window.isMapLoaded = false;
+
+    // Set map callback function
+    window.onMapLoaded = onMapLoaded;
+
+    // Set timeout for 10 seconds
+    setTimeout(() => {
+      if (!window.isMapLoaded) {
+        this.props.onError();
+      }
+    }, 10000);
+  }
+
+  render() {
+    return <div
+      role='region'
+      aria-label='map'
+      className='map-container'
+      style={{marginLeft: '250px'}}>
+      <MapComponent
+        isMarkerShown={this.props.places.length > 0}
+        googleMapURL='https://maps.googleapis.com/maps/api/js?key=AIzaSyCJ_6RgiFLqoJnPvPl2sKu3ETuSfjfmTFM&v=3.exp&libraries=geometry,drawing,places&callback=onMapLoaded'
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+        places={this.props.places}
+        hideInfoWindow={this.props.hideInfoWindow}
+        onMarkerClick={this.props.onMarkerClick}
+      />
+    </div>;
+  }
+}
+
+export default Map;
